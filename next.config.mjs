66a8206e589f1e88719f535ffa47941756264ac5
Anderson Fernandes <anderson.fernandes96@outlook.com>
2026-07-06 22:29:08 -0300

    chore(deps): commit library updates (package.json, next.config.mjs, tsconfig.json)

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['github.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+    ],
+    qualities: [75, 100],
   },
 }
 
